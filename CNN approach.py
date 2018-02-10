23e05b43741fea28815510aeb53fbf74bfdbfa3d
PragyanSubedi <pragyansubedi@gmail.com>
2018-02-10 17:04:50 +0545

    Added convolutional layers

--- CNN approach.py
+++ CNN approach.py
@@ -41,4 +41,38 @@ def convolutional_layer(input_x,shape):
 # NORMAL (FULLY CONNECTED)
 
 def normal_full_layer(input_layer,size):
-    input_size = int(input_layer.shape) 
+    input_size = int(input_layer.get_shape()[1])
+    W = init_weights([input_size,size])
+    b = init_bias([size])
+    return tf.matmul(input_layer,W) + b
+
+# PLACEHOLDERS
+
+x = tf.placeholder(tf.float32,shape = [None,784])
+
+y_true = tf.placeholder(tf.float32,shape=[None,10]) #AS one hot encoded
+
+# LAYERS
+# RESHAPE X TO 28 x 28. 1 is grayscale.
+x_image = tf.reshape(x, [-1,28,28,1])
+# 5 x 5 is patch size. 32 is no. of output channels (features computing)
+convo_1 = convolutional_layer(x_image,shape=[5,5,1,32])
+
+convo_1_pooling = max_pool_2by2(convo_1)
+
+convo_2 = convolutional_layer(convo_1_pooling,shape = [5,5,32,64])
+
+convo_2_pooling = max_pool_2by2(convo_2)
+
+# 7 by 7 image times output i.e 64
+
+convo_2_flat = tf.reshape(convo_2_pooling,[-1,7*7*64])
+
+# 1024 neurons
+
+full_layer_one = tf.nn.relu(normal_full_layer(convo_2_flat,1024))
+
+# DROPOUT
+
+hold_prob = tf.placeholder()
+
